1b65c7902b38ec1055d6913a78cdedbcf3ad61c3
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-17 10:44:13 +0300

    weather

--- control_flow.rb
+++ control_flow.rb
@@ -5,3 +5,17 @@ def admin_login(username, password)
     "Access denied"
   end
 end
+
+
+def hows_the_weather(temperature)
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
+end
+
