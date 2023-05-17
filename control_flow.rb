8f8187fd789b0aad3ad709a49167d1094e2fa4b8
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-17 10:44:50 +0300

    fizzbuzz

--- control_flow.rb
+++ control_flow.rb
@@ -19,3 +19,15 @@ def hows_the_weather(temperature)
   end
 end
 
+
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
+  end
+end
